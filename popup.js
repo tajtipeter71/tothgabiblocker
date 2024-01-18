d6aca601ac71d0987010c0a8bc7177eae8f55a79
Péter Tajti <tpeter@newline.hu>
2024-01-18 15:39:05 +0100

    release1.4.3

--- popup.js
+++ popup.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkbox = document.querySelector('input[type="checkbox"]');
+  var hState = document.getElementById("state");
 
-  // Kezdeti állapot lekérdezése
+  // Kezdeti állapot lekérdezése és beállítása
   chrome.storage.local.get('isTGExtensionActive', function(data) {
     checkbox.checked = data.isTGExtensionActive !== false;
+    hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :)";
   });
 
   // Checkbox állapotának kezelése és oldal újratöltése
   checkbox.addEventListener('change', function() {
     chrome.storage.local.set({ isTGExtensionActive: checkbox.checked }, function() {
+      hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :(";
       // Küld egy üzenetet a content scriptnek
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.reload(tabs[0].id);
