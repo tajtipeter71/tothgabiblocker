8ff1f3139be342c474f4bd6f060a1ce3d6a749c2
Péter Tajti <tpeter@newline.hu>
2024-09-03 09:30:04 +0200

    asd

--- popup.js
+++ popup.js
@@ -3,13 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
   var hState = document.getElementById("state");
 
   // Kezdeti állapot lekérdezése és beállítása
-  chrome.storage.local.get('isTGExtensionActive', function(data) {
-    checkbox.checked = data.isTGExtensionActive !== false;
-    hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :)";
-  });
+  let isTGExtensionActive = localStorage.getItem('isTGExtensionActive');  
+  checkbox.checked = isTGExtensionActive !== false;
+  hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :)";
+
+  // chrome.storage.local.get('isTGExtensionActive', function(data) {
+  //   checkbox.checked = data.isTGExtensionActive !== false;
+  //   hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :)";
+  // });
 
   // Checkbox állapotának kezelése és oldal újratöltése
   checkbox.addEventListener('change', function() {
+    localStorage.setItem('isTGExtensionActive', checkbox.checked);
+    hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :)";
+    
     chrome.storage.local.set({ isTGExtensionActive: checkbox.checked }, function() {
       hState.textContent = checkbox.checked ? "Nincs Tóth Gabi :)" : "Van Tóth Gabi :(";
       // Küld egy üzenetet a content scriptnek
